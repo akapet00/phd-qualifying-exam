3abad2c46aa2d45a3418f1dc40639a4d7787925b
antelk <alojic00@fesb.hr>
2022-12-28 12:00:28 +0100

    update functionality and generate figures

--- code/spherical_model.py
+++ code/spherical_model.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
@@ -11,46 +13,47 @@ from utils import set_axes_equal
 
 def main():
     # coordinates
-    r = 50 / 1000  # in m
-    theta = np.linspace(0, 2 * np.pi, 17)
-    phi = np.linspace(0, np.pi, 9)
+    r = 0.05  # in m
+    theta = np.linspace(0, 2 * np.pi, 16)
+    phi = np.linspace(0, np.pi, 8)
     Theta, Phi = np.meshgrid(theta, phi)
     x, y, z = sph2cart(r, Theta.ravel(), Phi.ravel())
+
+    # smooth spherial surface
     hull = spatial.ConvexHull(np.c_[x, y, z])
 
+    # traingular mesh
+    hull_triangle_coords = hull.points[hull.simplices]
+
     # visualize
-    update_matplotlib_rc_parameters()
+    update_matplotlib_rc_parameters(is_3d=True)
     cs = sns.color_palette('rocket', 2)
     fig = plt.figure()
     ax = plt.axes(projection ='3d')
-    ax.scatter(x, y, z, ec='k', color=cs[0])
-    hull_triangle_coords = hull.points[hull.simplices]
-    triangles = Poly3DCollection(hull_triangle_coords,
-                                color=cs[1], ec='k', lw=0.1, alpha=0.25)
+    triangles = Poly3DCollection(hull_triangle_coords, lw=0.5,
+                                 color=cs[1], alpha=0.25)
     ax.add_collection3d(triangles)
+    ax.scatter(x, y, z, color=cs[0])
     ax.set_box_aspect([1, 1, 1])
     ax = set_axes_equal(ax)
+    ticks = [-r, 0, r]
+    ticklabels = [-round(r * 1000), 0, round(r * 1000)]
+    lim = [-r * 1.2, r * 1.2]
     ax.set(xlabel='$x$ [mm]', ylabel='$y$ [mm]', zlabel='$z$ [mm]',
-        xticks=[-r, 0, r],
-        yticks=[-r, 0, r],
-        zticks=[-r, 0, r],
-        xticklabels=[-int(r * 1000), 0, int(r * 1000)],
-        yticklabels=[-int(r * 1000), 0, int(r * 1000)],
-        zticklabels=[-int(r * 1000), 0, int(r * 1000)],
-        xlim=[-r * 1.2, r * 1.2],
-        ylim=[-r * 1.2, r * 1.2],
-        zlim=[-r * 1.2, r * 1.2],
-        )
+           xticks=ticks, yticks=ticks, zticks=ticks,
+           xticklabels=ticklabels,
+           yticklabels=ticklabels,
+           zticklabels=ticklabels,
+           xlim=lim, ylim=lim, zlim=lim
+           )
     ax.xaxis.pane.fill = False
     ax.yaxis.pane.fill = False
     ax.zaxis.pane.fill = False
-    ax.xaxis.labelpad = 7
-    ax.yaxis.labelpad = 7
-    ax.zaxis.labelpad = 7
-    ax.view_init(25, -55)
+    ax.view_init(20, -60)
+    fig.tight_layout()
     # fig.savefig(os.path.join(
     #     os.pardir, 'artwork', 'spherical_model.pdf'
-    #     ), bbox_inches='tight')
+    #     ))
     plt.show()
 
 
--- code/spherical_surface.py
+++ code/spherical_surface.py
@@ -13,70 +13,68 @@ from utils import sph_normals
 
 
 def main():
+	# averaging surface configuration
 	edge_length = 0.02  # in m
 	target_area_origin = (-edge_length/2, -edge_length/2)
-	N = 7  # number of grid points
+	N = 11  # number of grid points
 
-	# planar control surface
+	# control evaluation plane coordinates
 	x = np.linspace(-edge_length/2, edge_length/2, N)
-	y = 0.0
+	y = 0
 	z = np.linspace(-edge_length/2, edge_length/2, N)
 	Xt, Zt = np.meshgrid(x, z)
 	x_pln = Xt.ravel()
 	z_pln = Zt.ravel()
 
-	# spherical averaging surface
-	r = 50 / 1000  # in m
-	alpha = 2 * np.arcsin(edge_length/2/r)  # compute angle from secant
-	theta = np.linspace(np.pi/2 - alpha/2, np.pi/2 + alpha/2, N)
+	# spherical averaging surface coordinates
+	r = 0.05  # in m
+	alpha = 2 * np.arcsin(edge_length/2/r)  # angle from secant
+	theta = np.linspace(np.pi/2-alpha/2, np.pi/2+alpha/2, N)
 	phi = np.linspace(np.pi-alpha/2, np.pi+alpha/2, N)
 	Theta, Phi = np.meshgrid(theta, phi)
 	y_sph, x_sph, z_sph = sph2cart(r, Theta.ravel(), Phi.ravel())
-	y_sph -= y_sph.min()
+	y_sph -= y_sph.min() + y_sph.ptp() / 2
 	ny_sph, nx_sph, nz_sph = sph_normals(r, Theta.ravel(), Phi.ravel())
 
 	# visualize
-	update_matplotlib_rc_parameters()
+	update_matplotlib_rc_parameters(is_3d=True)
 	cs = sns.color_palette('rocket', 2)
 	fig = plt.figure()
 	ax = plt.axes(projection ='3d')
 	plane = Rectangle(target_area_origin,
-					width=edge_length, height=edge_length,
-					ec=cs[0], fc=cs[1], alpha=0.25,
-					label='control evaluation plane')
+					  width=edge_length, height=edge_length,
+					  ec=cs[0], fc=cs[1], alpha=0.25,
+					  label='control evaluation plane')
 	ax.add_patch(plane)
 	pathpatch_2d_to_3d(plane, z=y, zdir='y')
 	ax.scatter(x_sph, y_sph, z_sph, color=cs[0], depthshade=True,
-		label='sampled spherical surface')
-	ax.quiver(x_sph, y_sph, z_sph, nx_sph, ny_sph, nz_sph, normalize=True,
-		arrow_length_ratio=0.33, lw=1.25, length=1/1000, color=cs[0],
-		label='$\hat{\mathbf{n}}$')
+		       label='integration grid')
+	ax.quiver(x_sph, y_sph, z_sph,
+		      nx_sph, ny_sph, nz_sph,
+			  normalize=True, arrow_length_ratio=0.33, length=0.75/1000,
+			  lw=1.25, color=cs[0], label='unit normal vector')
 	ax.set_box_aspect([1, 1, 1])
 	ax.set(xlabel='$x$ [mm]', ylabel='$y$ [mm]', zlabel='$z$ [mm]',
-		xticks=[x_pln.min(), 0.0, x_pln.max()],
-		yticks=[y_sph.min(), y_sph.ptp()/2, y_sph.max()],
-		zticks=[z_pln.min(), 0.0, z_pln.max()],
-		xticklabels=[int(x_pln.min()*1000), 0, int(x_pln.max()*1000)],
-		yticklabels=[round(y_sph.min()*1000, 2),
-					round(y_sph.ptp()/2*1000, 2),
-					round(y_sph.max()*1000, 2)],
-		zticklabels=[int(z_pln.min()*1000), 0, int(z_pln.max()*1000)],
-		xlim=[x_sph.min()*1.1, x_sph.max()*1.1],
-		ylim=[y_sph.min() - abs(y_sph.max() * 0.1),
-				y_sph.max() + abs(y_sph.max() * 0.1)],
-		zlim=[z_sph.min()*1.1, z_sph.max()*1.1],
-		)
+		   xticks=[x_pln.min(), 0.0, x_pln.max()],
+		   yticks=[y_sph.min(), 0.0, y_sph.max()],
+		   zticks=[z_pln.min(), 0.0, z_pln.max()],
+		   xticklabels=[round(x_pln.min()*1000), 0, round(x_pln.max()*1000)],
+		   yticklabels=[round(y_sph.min()*1000, 2), 0, round(y_sph.max()*1000, 2)],
+		   zticklabels=[round(z_pln.min()*1000), 0, round(z_pln.max()*1000)],
+		   xlim=[x_pln.min()*1.5, x_pln.max()*1.5],
+		   ylim=[y_sph.min()-abs(y_sph.max()*0.5),
+				 y_sph.max()+abs(y_sph.max()*0.5)],
+		   zlim=[z_pln.min()*1.5, z_pln.max()*1.5]
+		   )
 	ax.xaxis.pane.fill = False
 	ax.yaxis.pane.fill = False
 	ax.zaxis.pane.fill = False
-	ax.xaxis.labelpad = 10
-	ax.yaxis.labelpad = 10
-	ax.zaxis.labelpad = 10
-	ax.view_init(25, -55)
+	ax.view_init(20, -70)
 	fig.legend()
+	fig.tight_layout()
 	# fig.savefig(os.path.join(
 	#     os.pardir, 'artwork', 'spherical_surface.pdf'
-	#     ), bbox_inches='tight')
+	#     ))
 	plt.show()
 
 
